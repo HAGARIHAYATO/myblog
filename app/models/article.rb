53f339a5109784cedb739bc84a74a83c6b9f90fd
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2019-09-29 00:08:25 +0900

    fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,6 @@ class Article < ApplicationRecord
 	has_rich_text :text
 	validates :title, presence: true
 	validates :text, presence: true
-
 	# belongs_to :user
 	belongs_to :category
 	has_many :comments, dependent: :destroy
